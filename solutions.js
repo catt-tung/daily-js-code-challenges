e66ce89db00b6abc1aed65e1da07f1f6756ff1f8
Catherine Tung <catt.tung@gmail.com>
2022-04-07 19:03:25 -0400

    Solution 22

--- solutions.js
+++ solutions.js
@@ -803,33 +803,7 @@ primeFactors(200) //=> [2, 2, 2, 5, 5]
 // Your solution for 21-primeFactors here:
 
 /*--- most logical approach ---*/
-function primeFactors(n) {
-  var factors = [];
-  if (n < 2 || !Number.isInteger(n)) return factors;
-  
-  // function to help find next prime to divide by...
-  function isPrime(n) {
-    if (n < 2 || !Number.isInteger(n)) return false;
-    for (var i = 2; i <= n / 2; i++) {
-      if (Number.isInteger(n / i)) return false;
-    }
-    return true;
-  }
-  
-  var prime = 2;  // start with smallest prime
-  while (!isPrime(n)) {
-    if (Number.isInteger(n / prime)) {
-      factors.push(prime);
-      n = n / prime;
-    } else {
-      // find next prime
-      prime++;
-      while (!isPrime(prime)) prime++;
-    }
-  }
-  factors.push(n);
-  return factors;
-}
+
 
 /*-- a more efficient algorithm that relies on the fact
      that you don't have to check if the divisor is prime 
